65f45bd87efed552d495707d1038f5697c187691
Shashi Shekhar <shashi.carbon27@gmail.com>
2017-01-19 21:32:04 +0530

    Blog CRUD complete

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
+// Middleware for Body Parser
+app.use(bodyParser.json());
+
 Post = require('./models/post');
 
 mongoose.connect('mongodb://localhost/blog');
@@ -23,13 +26,47 @@ app.get('/api/posts', function(req, res) {
     });
 });
 
+// Add post
+app.post('/api/posts', function(req, res) {
+   var post_data = req.body;
+    Post.addPost(post_data, function(err, post) {
+        if (err) {
+            throw  err;
+        }
+        res.json(post);
+    }); 
+});
+
+// Update post
+app.put('/api/posts/:_id', function(req, res) {
+    var id = req.params._id;
+   var post_data = req.body;
+    Post.updatePost(id, post_data, {}, function(err, post) {
+        if (err) {
+            throw  err;
+        }
+        res.json(post);
+    }); 
+});
+
+
+// Remove post
+app.delete('/api/posts/:_id', function(req, res) {
+   var id = req.params._id;
+    Post.removePost(id, function(err, post) {
+        if (err) {
+            throw err;
+        }
+        res.json(post);
+    });
+});
+
 // Get Single post
 app.get('/api/post/:_id', function(req, res) {
     Post.getPostById(req.params._id, function(err, post) {
         if (err) {
             throw err;
         }
-        
         res.json(post);
     });
 });
--- models/post.js
+++ models/post.js
@@ -5,6 +5,10 @@ var postSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    slug: {
+        type: String,
+        required: true
+    },
     body: {
         type: String,
         required: true
@@ -39,4 +43,29 @@ module.exports.getPosts = function(callback, limit) {
 // Get single post
 module.exports.getPostById = function(_id, callback) {
     Posts.findById(_id, callback);
+}
+
+// Add post
+module.exports.addPost = function(post_obj, callback) {
+    Posts.create(post_obj, callback);
+}
+
+// Update post
+module.exports.updatePost = function(id, post, options, callback) {
+    var query = {_id: id};
+    var update = {
+        title: post.title,
+        slug: post.slug,
+        body: post.body,
+        category: post.category,
+        author: post.author
+    }
+    
+    Posts.findOneAndUpdate(query, update, options, callback);
+}
+
+// Remove post
+module.exports.removePost = function(id, callback) {
+    var query = {_id: id};
+    Posts.remove(query, callback);
 }
